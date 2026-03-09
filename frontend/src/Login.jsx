8abe25a4c0047addec3e3f02c1bf00da3108efdc
Anna Norris <alnorris113@gmail.com>
2026-03-08 23:15:54 -0700

    made the login beautiful

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -28,9 +28,47 @@ export default function Login() {
           <p id="beta">beta</p>
         </header>
 
-        <section id="auth">
-          <button id="loginBtn" onClick={handleLogin}>Continue with Google</button>
-        </section>
+        <body>
+          <section id="auth">
+            <button id="loginBtn" onClick={handleLogin}>Continue with Google!</button>
+          </section>
+          <div className="columns">
+            <div className='column'>
+              <h3>Import Your Google Calendars</h3>
+              <p>
+                With Social Scheduler, you can import your Google calendars to 
+                start making scheduling easier. Just press the Continue with Google 
+                to create a new account and choose which calendars you want to use for 
+                scheduling! You'll be able to add your own special events without using
+                Google calendar too. 
+              </p>
+              <p>
+                Try clicking on any event to set its priority!
+              </p>
+            </div>
+            <div className='column'>              
+              <h3>Create Groups</h3>
+              <p>
+                Have you ever struggled with scheduling study sessions or hangouts With
+                your friends and classmates? This website aims to make the process that much
+                smoother. You can create groups and invite people to them. Send them a shareable 
+                link to invite them, or, if you know their username, let the app send them an email 
+                for you!
+              </p>
+              </div>
+            <div className='column'>
+              <h3>Petition a Time</h3>
+              <p>
+                With an existing group, you can "petition" your group for a time to meet up. 
+                View your group's availability and find a time where everyone can meet based on
+                how much you want to prioritize your meetup.
+                Everybody can stay on the same page as everyone gets to see exactly where the 
+                petitioned time lands on their calendar. All of your group members can accept or 
+                decline a petition.
+              </p>
+            </div>
+          </div>
+        </body>
       </div>
       </>
   );
