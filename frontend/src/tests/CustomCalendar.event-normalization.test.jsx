fece2a90910bad200658aca00f5bed63db45bc9e
Anna Norris <alnorris113@gmail.com>
2026-03-10 14:33:18 -0700

    moved test files

--- frontend/src/tests/CustomCalendar.event-normalization.test.jsx
+++ frontend/src/tests/CustomCalendar.event-normalization.test.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { act } from 'react';
 
-import CustomCalendar from './CustomCalendar';
-import { apiGet, apiPost } from '../../api';
+import CustomCalendar from '../components/Calendar/CustomCalendar';
+import { apiGet, apiPost } from '../api';
 
 jest.mock('../../api', () => ({
   apiGet: jest.fn(),
